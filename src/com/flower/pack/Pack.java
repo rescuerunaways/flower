5bd98beed37248f651759073dd481c386a677870
Your Name <ermolaeva@agent.ru>
2017-04-29 23:07:58 +1000

    main packing function, basic test

--- src/com/flower/pack/Pack.java
+++ src/com/flower/pack/Pack.java
@@ -1,8 +1,50 @@
 package com.flower.pack;
 
-public class Pack {
+import java.util.ArrayList;
+import java.util.List;
 
-    int[] pack(int order, int[] bundles) {
-        return new int[]{42};
+public class Pack {
+    public List<Integer> pack(int order, int[] bckts) {
+        int[] bcktCache = new int[order + 1];
+        int[] minBcktCache = new int[order + 1];
+
+        for (int i = 0; i <= order; i++) {
+            int bcktCount = i;
+            int newBckt = 1;
+
+
+            for (int j : getSmallerBckts(bckts, i)) {
+                //TODO add case for equal number, but different price
+                if (minBcktCache[i - j] + 1 < bcktCount) {
+                    bcktCount = minBcktCache[i - j] + 1;
+                    newBckt = j;
+                }
+            }
+
+            minBcktCache[i] = bcktCount;
+            bcktCache[i] = newBckt;
+        }
+
+        List<Integer> result = new ArrayList<>(order);
+
+        while (order > 0) {
+            int thisBckt = bcktCache[order];
+            result.add(thisBckt);
+            order = order - thisBckt;
+        }
+
+        return result;
+    }
+
+
+    private int[] getSmallerBckts(int[] bckts, int i) {
+        int[] smaller = new int[bckts.length];
+
+        for (int k = 0; k <= bckts.length - 1; k++) {
+            if (bckts[k] <= i) {
+                smaller[k] = bckts[k];
+            }
+        }
+        return smaller;
     }
 }
--- tests/com/flower/pack/TestPack.java
+++ tests/com/flower/pack/TestPack.java
@@ -3,11 +3,14 @@ package com.flower.pack;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class TestPack {
     @Test()
     public void testPack() {
         Pack pack = new Pack();
-        int[] result = pack.pack(5, new int[]{1, 2, 3});
-        Assert.assertArrayEquals(result, new int[]{42});
+        List<Integer> result = pack.pack(5, new int[]{1, 2, 3});
+        Assert.assertEquals(Arrays.asList(2,3),result);
     }
 }
