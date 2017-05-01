0bb7b43c06ab3308e23f135e30a10bbe282fdcc5
Your Name <ermolaeva@agent.ru>
2017-05-01 23:21:20 +1000

    formatting

--- src/com/flower/pack/io/Input.java
+++ src/com/flower/pack/io/Input.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Input {
-
     public static List<String> getOrders() throws IOException {
         return Files.lines(Paths.get("./order/order.txt")).collect(Collectors.toList());
     }
