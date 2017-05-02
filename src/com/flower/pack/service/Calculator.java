a09f08cdfb8bef2adf9ebccfa47d2d2d20eeee90
Your Name <ermolaeva@agent.ru>
2017-05-02 12:43:38 +1000

    BigDecimal

--- src/com/flower/pack/service/Calculator.java
+++ src/com/flower/pack/service/Calculator.java
@@ -2,14 +2,15 @@ package com.flower.pack.service;
 
 import com.flower.pack.model.Order;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
 import static com.flower.pack.model.Store.flowers;
 
 public class Calculator {
-    public static Double sum(Order o, List<Integer> pack) {
-        Map<Integer, Double> buckets = flowers.get(o.getCode());
-        return pack.stream().map(buckets::get).reduce(0.0, Double::sum);
+    public static BigDecimal sum(Order o, List<Integer> pack) {
+        Map<Integer, BigDecimal> buckets = flowers.get(o.getCode());
+        return pack.stream().map(buckets::get).reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 }
--- tests/com/flower/pack/service/CalculatorTest.java
+++ tests/com/flower/pack/service/CalculatorTest.java
@@ -4,17 +4,18 @@ import com.flower.pack.model.Order;
 import com.flower.pack.model.Store;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.Assert.assertEquals;
 
 
 public class CalculatorTest {
 
     @Test()
-    public void testPack() {
-        Double result = Calculator.sum(new Order(20, "R12"), Pack.pack(20, Store.getBuckets("R12")));
-        assertEquals(Double.parseDouble("25.98"), result, 0);
+    public void testSum() {
+        BigDecimal result = Calculator.sum(new Order(20, "R12"), Pack.pack(20, Store.getBuckets("R12")));
+        assertEquals(new BigDecimal(25.98), result);
     }
-
 }
 
 
